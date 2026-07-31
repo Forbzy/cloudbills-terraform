042b64bd31c082f833398213191a7f28133a74e7
James Forbes <james_forbes@live.co.uk>
2026-08-01 04:58:14 +0800

    added security_list

--- terraform/oracleCloud/modules/networking/main.tf
+++ terraform/oracleCloud/modules/networking/main.tf
@@ -156,7 +156,7 @@ resource "oci_core_subnet" "private_subnet" {
 }
 
 resource "oci_core_security_list" "worker_security_list" {
-  compartment_id = var.compartment_id
+  compartment_id = var.compartment_ocid
   vcn_id         = var.vcn_id
   display_name   = "oke-worker-security-list"
 
