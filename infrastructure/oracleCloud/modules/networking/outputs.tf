b3990b71440d266bdaae959653bd073c54271390
James Forbes <james_forbes@live.co.uk>
2026-08-06 01:13:17 +0800

    github token

--- infrastructure/oracleCloud/modules/networking/outputs.tf
+++ infrastructure/oracleCloud/modules/networking/outputs.tf
@@ -12,3 +12,8 @@ output "public_subnet_id" {
   description = "Public subnet OCID"
   value       = oci_core_subnet.public_subnet.id
 }
+
+output "bastion_id" {
+  value       = oci_bastion_bastion.flux_bastion.id
+  description = "The OCID of the newly created OCI Bastion."
+}
